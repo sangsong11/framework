3c14228f4be8045d89d263387374d9411583d3b8
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2020-10-26 21:50:22 +0800

    Add files via upload

--- leetcode/repeatedna.c
+++ leetcode/repeatedna.c
@@ -5,9 +5,11 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 using namespace std;
 
-
+#define MAX_LEN 1024
 class Solution
 {
 public:
@@ -40,7 +42,69 @@ public:
        return result;
     }
 
+    
+
 };
+
+
+
+int change_to_array(const char *str,int a[])
+{
+   int len = strlen(str);
+   int i = 0;
+   int j = 0;
+
+   for(i = len -1 ; i >= 0; i--)
+   {
+      a[j++] = str[i]-'0';
+   }
+   return j;
+}
+
+
+char *addBinary(char *a, char *b)
+{
+   int n1[MAX_LEN] = {0};
+   int n2[MAX_LEN] = {0};
+
+   int nlen1 = change_to_array(a,n1);
+   int nlen2 = change_to_array(b,n2);
+
+  int max_len = nlen1 > nlen2 ? nlen1:nlen2;
+  int result[MAX_LEN] = {0};
+  int result_len = 0;
+  int add_one = 0;
+  int i;
+
+  for(i = 0;i< max_len;i++)
+  {
+    int bit = n1[i] + n2[i] + add_one;
+    if(bit >= 2)
+    {
+      add_one = 1;
+    }
+    else
+    {
+      add_one = 0;
+    }
+    result[result_len++] = bit % 2;
+  } 
+  
+  if(add_one == 1)
+  {
+    result[result_len++] = 1;
+  }
+
+  char *result_str = (char*)malloc(MAX_LEN * sizeof(char));
+  int strn = 0;
+  
+  for(i = result_len -1; i>=0;i--)
+  {
+    result_str[strn++] = result[i]+'0';
+  }
+ result_str[strn] = '\0';
+ return result_str;
+}
 
 int main(int argc,char **argv)
 {
@@ -53,5 +117,13 @@ int main(int argc,char **argv)
     {
          printf("%s\n",result[i].c_str());
     }
+
+
+    char a[] = "11";
+    char b[] = "1";
+
+    char *output = addBinary(a,b);
+    printf("%s + %s = %s\n",a,b,output);
+    free(output);
     return 0;
 }
