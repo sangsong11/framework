ec3e6bf50f7f7bf96018a1cb94454e910fd3b010
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2020-10-29 22:14:43 +0800

    Add files via upload

--- leetcode/repeatedna.c
+++ leetcode/repeatedna.c
@@ -13,6 +13,9 @@ using namespace std;
 class Solution
 {
 public:
+   /*leetcode 重复的DNA序列， 已知一段DNA序列，它有A、C 、G、和T四种字符代表的核苷酸组成，在计算机中DNA序列可以用字符串
+    表示，现在有一段字符串表示DNA序列，我们需要从该字符串中找出长度为10，并在序列中至少出现2次的子串*/
+
     vector<string>findRepeatedDnaSequences(string s)
     {
        //
@@ -42,7 +45,8 @@ public:
        return result;
     }
 
-  int coinChange(vector<int>& coins,int amount)
+  /*leetcode 322 找零钱 已知一些不同面值的钞票与一个金额，如何用最少个数钞票组成这个金额，失败返回-1，否则返回个数*/
+  int coinChange(vector<int>& coins,int amount)// 1:数组钞票的面值 2：金额数
   {
      vector<int>dp(amount+1,-1);
      dp[0] = 0;
@@ -60,13 +64,38 @@ public:
          }
      }
 
-    return dp[amount];
-  }  
+    return dp[amount]; //返回最小的张数
+  } 
 
+/*leetcode198 房间寻宝  在一条直线上，有n个房屋，每个房屋中有数量不等的财宝，有一个探险家希望从房间中获取财宝
+房屋中有报警器，如果同时从相邻的两个房屋中获取财宝就会触发警报，那么在不触发警报的前提下，最多获取多少财宝*/  
+  int rob(vector<int>&nums)
+  {
+      int n = nums.size();
+      if(n == 0)
+      {
+        return 0;
+      }
+      if(n == 1)
+      {
+        return nums[0];
+      }
+
+      vector<int> value(n,0);
+      value[0] = nums[0];
+      
+      value[1] = max(nums[0],nums[1]);
+      for(int i = 2;i < n;i++)
+      {
+        value[i] = max(value[i-1],value[i-2]+nums[i]);
+      }
+
+     return value[n-1];
+  }
 };
 
 
-
+/*leeetcode 字符串表示二进制大整数加法*/
 int change_to_array(const char *str,int a[])
 {
    int len = strlen(str);
@@ -156,6 +185,17 @@ int main(int argc,char **argv)
 
     printf("dp %d\n",solution.coinChange(coins,14));
 
+   
 
+   vector<int>nums;
+   nums.push_back(1);
+   nums.push_back(3);
+   nums.push_back(4);
+   nums.push_back(3);
+   nums.push_back(0);
+   nums.push_back(2);
+   nums.push_back(7);
+
+   printf("%d\n",solution.rob(nums));
     return 0;
 }
