70599ab05ecd7287832a3994fa18922fec27e49c
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2020-12-27 22:43:35 +0800

    Update test_rsa_evp.cpp

--- openssl/test_rsa_evp.cpp
+++ openssl/test_rsa_evp.cpp
@@ -1,27 +1,3 @@
-﻿/*/*******************************************************************************
-**                                                                            **
-**                     Jiedi(China nanjing)Ltd.                               **
-**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
-*******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
-**
-** Project       :c++实战区块链核心密码学-基于openssl
-** Contact       : xiacaojun@qq.com
-**  博客   : http://blog.csdn.net/jiedichina
-**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
-			   腾讯课堂		https://jiedi.ke.qq.com/				
-			   csdn学院               http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
-**             51cto学院              http://edu.51cto.com/lecturer/index/user_id-12016059.html	
-** 			   老夏课堂		http://www.laoxiaketang.com 
-**                 
-**  c++实战区块链核心密码学-基于openssl   课程群 ：1064420127加入群下载代码和学员交流
-**                           微信公众号  : jiedi2007
-**		头条号	 : 夏曹俊
-**
-*****************************************************************************
-//！！！！！！！！！c++实战区块链核心密码学-基于openssl 课程  QQ群：1064420127下载代码和学员交流*/
-
 #include <iostream>
 #include <openssl/rsa.h>
 #include <openssl/err.h>
