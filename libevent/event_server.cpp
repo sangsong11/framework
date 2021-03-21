3cb843e0087d0da28fc3d3e3915a87f4df8b2c57
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2021-03-21 21:34:29 +0800

    Add files via upload

---
+++ libevent/event_server.cpp
@@ -0,0 +1,147 @@
+/*/*******************************************************************************
+**                                                                            **
+**                     Jiedi(China nanjing)Ltd.                               **
+**	               创建：丁宋涛 夏曹俊，此代码可用作为学习参考                **
+*******************************************************************************/
+
+/*****************************FILE INFOMATION***********************************
+**
+** Project       : Libevent C＋＋高并发网络编程
+** Contact       : xiacaojun@qq.com
+**  博客   : http://blog.csdn.net/jiedichina
+**	视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
+			   腾讯课堂		https://jiedi.ke.qq.com/				
+			   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
+**             51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
+** 			   老夏课堂		http://www.laoxiaketang.com 
+**                 
+**  Libevent C＋＋高并发网络编程 课程群 ：1003847950 加入群下载代码和交流
+**   微信公众号  : jiedi2007
+**		头条号	 : 夏曹俊
+**
+*****************************************************************************
+//！！！！！！！！！ Libevent C＋＋高并发网络编程 课程  QQ群：1003847950 下载代码和交流*/
+#include <iostream>
+#include <event2/event.h>
+#ifndef _WIN32
+#include <signal.h>
+#else
+#endif
+#include <thread>
+#include <errno.h>
+#include <string.h>
+using namespace std;
+#define SPORT 5001 
+
+//正常断开连接也会进入，超时会进入
+void client_cb(evutil_socket_t s,short w, void *arg)
+{
+	//水平触发LT 只有有数据没有处理，会一直进入
+	//边缘触发ET 有数据时只进入一次
+	//cout<<"."<<flush;return;
+	
+	event *ev = (event *)arg;
+	//判断超时
+	if(w&EV_TIMEOUT)
+	{
+		cout<<"timeout"<<endl;
+		event_free(ev);
+		evutil_closesocket(s);
+		return;
+	}
+	
+	//char buf[1024] = {0};
+	char buf[1024] = {0};
+	int len = recv(s,buf,sizeof(buf)-1,0);
+	if(len>0)
+	{
+		cout<<buf<<endl;
+		send(s,"ok",2,0);
+	}
+	else
+	{
+		//需要清理event
+		cout<<"event_free"<<flush;
+		event_free(ev);
+		evutil_closesocket(s);
+		
+	}
+	
+}
+
+void listen_cb(evutil_socket_t s,short w, void *arg)
+{
+	cout<<"listen_cb"<<endl;
+	sockaddr_in sin;
+	socklen_t size = sizeof(sin);
+	//读取连接信息
+	evutil_socket_t client = accept(s,(sockaddr*)&sin,&size);
+	char ip[16] = {0};
+	evutil_inet_ntop(AF_INET,&sin.sin_addr,ip,sizeof(ip)-1);
+	cout<<"client ip is "<<ip<<endl;
+	
+	//客户端数据读取事件
+	event_base *base = (event_base *)arg;
+	event *ev = event_new(base,client,EV_READ|EV_PERSIST,client_cb,event_self_cbarg());
+	//边缘触发
+	//event *ev = event_new(base,client,EV_READ|EV_PERSIST|EV_ET,client_cb,event_self_cbarg());
+	timeval t = {10,0};
+	event_add(ev,&t);
+}
+
+int main(int argc,char *argv[])
+{
+#ifdef _WIN32 
+	//初始化socket库
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2,2),&wsa);
+#else
+	//忽略管道信号，发送数据给已关闭的socket
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
+		return 1;
+#endif
+
+	event_base *base = event_base_new();
+	
+	//event 服务器
+	cout<<"test event server"<<endl;
+	
+	//创建socket
+	evutil_socket_t sock = socket(AF_INET,SOCK_STREAM,0);
+	if(sock<=0)
+	{
+		cerr<<"socket error："<<strerror(errno)<<endl;
+		return -1;
+	}
+	int val = 1;
+	
+	//设置地址复用和非阻塞
+	evutil_make_socket_nonblocking(sock);
+	evutil_make_listen_socket_reuseable(sock);
+	//绑定端口和地址
+	sockaddr_in sin;
+	memset(&sin,0,sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(SPORT);
+	int re = ::bind(sock,(sockaddr*)&sin,sizeof(sin));
+	if(re != 0)
+	{
+		cerr<<"bind error:"<<strerror(errno)<<endl;
+		return -1;
+	}
+	//开始监听
+	listen(sock,10);
+	
+	//开始接受连接事件 默认水平触发
+	event *ev = event_new(base,sock,EV_READ|EV_PERSIST,listen_cb,base);
+	event_add(ev,0);
+	
+	
+	
+	//进入事件主循环
+	event_base_dispatch(base);
+	evutil_closesocket(sock);
+	event_base_free(base);
+		
+    return 0;
+}
