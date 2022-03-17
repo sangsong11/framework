69d2a78863e87c8073f94747cf5e28c357268fdc
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2022-03-17 13:31:20 +0800

    Update command.sh

--- linux/command.sh
+++ linux/command.sh
@@ -5,3 +5,9 @@ ulimit -c unlimited
 tailf log/log_center.txt
 
 patchelf 去掉开源动态库的soname
+
+网络端口转发：
+    iptables -t filter -P FORWARD ACCEPT
+    vi /etc/sysctl.conf  ->    net.ipv4.ip_forward =1
+    iptables -t nat -A PREROUTING -d 30.30.3.216 -p tcp -dport 9092 -j DNAT --to 192.0.24.221:22
+    iptables -t nat -A POSTROUTING -p -tcp -j MASQUREAD
