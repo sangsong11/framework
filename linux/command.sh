1a8f9067cc831cf251b9f0748d356fb7fc3339d2
sangsong11 <38214336+sangsong11@users.noreply.github.com>
2022-03-17 13:21:30 +0800

    Update command.sh

--- linux/command.sh
+++ linux/command.sh
@@ -1,2 +1,5 @@
 valgrind --error-limit=no --tool=memcheck --leak-check=full --track-origins=yes --show-reachable=yes --log-file=./mem.log
 tcpdump -i eth0 'port 3000' -X -c 3
+ulimit -c unlimited
+
+tailf log/log_center.txt
